a5f6ae6d6de0cfbb4ca50db17697a0e51dcccf2a
karlos1998 <fadeusz98@gmail.com>
2024-10-30 11:30:18 +0100

    try to fix audio_devices.py

--- audio_devices.py
+++ audio_devices.py
@@ -1,5 +1,3 @@
-from threading import Thread
-from time import sleep
 import sounddevice as sd
 import uuid
 
@@ -7,12 +5,11 @@ class AudioDevices:
     devices = []
 
     @staticmethod
-    def __generate_uuid(device):
+    def __generate_uuid(device, role):
         unique_string = (
             f"{device['name']}-{device['hostapi']}-"
-            f"{device['max_input_channels']}-{device['max_output_channels']}-"
-            f"{device['default_low_input_latency']}-{device['default_low_output_latency']}-"
-            f"{device['default_high_input_latency']}-{device['default_high_output_latency']}-"
+            f"{device['max_input_channels'] if role == 'input' else device['max_output_channels']}-"
+            f"{device['default_low_input_latency'] if role == 'input' else device['default_low_output_latency']}-"
             f"{device['default_samplerate']}"
         )
         return uuid.uuid5(uuid.NAMESPACE_DNS, unique_string).__str__()
@@ -21,19 +18,24 @@ class AudioDevices:
     def get_list():
         devices = sd.query_devices()
         AudioDevices.devices = []
-        for i, device in enumerate(devices):
-            if device['max_input_channels'] > 0 and device['max_output_channels'] > 0:
-                device_type = "hybrid"
-            elif device['max_input_channels'] > 0:
-                device_type = "input"
-            else:
-                device_type = "output"
 
-            device_uuid = AudioDevices.__generate_uuid(device)
-            device["type"] = device_type
-            device["uuid"] = device_uuid
-            device["index"] = i  # Dodajemy index urządzenia
-            AudioDevices.devices.append(device)
+        for i, device in enumerate(devices):
+            # Tworzenie wpisu dla funkcji wejściowej, jeśli urządzenie ma wejścia
+            if device['max_input_channels'] > 0:
+                input_device = device.copy()
+                input_device["type"] = "input"
+                input_device["uuid"] = AudioDevices.__generate_uuid(device, "input")
+                input_device["index"] = i  # Zapisz indeks dla referencji
+                AudioDevices.devices.append(input_device)
+
+            # Tworzenie wpisu dla funkcji wyjściowej, jeśli urządzenie ma wyjścia
+            if device['max_output_channels'] > 0:
+                output_device = device.copy()
+                output_device["type"] = "output"
+                output_device["uuid"] = AudioDevices.__generate_uuid(device, "output")
+                output_device["index"] = i  # Zapisz indeks dla referencji
+                AudioDevices.devices.append(output_device)
+
         return AudioDevices.devices
 
     @staticmethod
