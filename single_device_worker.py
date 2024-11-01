d810024d826cc958a3a300d2a1b768420a115ceb
karlos1998 <fadeusz98@gmail.com>
2024-11-01 18:44:12 +0100

    fix run call

--- single_device_worker.py
+++ single_device_worker.py
@@ -87,7 +87,7 @@ class SingleDeviceWorker:
                 if node is not None and self.conversation_tree is None:
                     print("URUCHAMIANIE DRZEWKA ROZMOWY")
                     self.conversation_tree = ConversationTree(self)
-                    time.sleep(1)
+                    time.sleep(3)
                     self.conversation_tree.run_node(node)
 
         if self.call_status == 'IDLE':
